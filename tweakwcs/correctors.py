e4d129769401ab0a62c6dab93b77509ba5b87f0f
Mihai Cara <mihail.cara@gmail.com>
2023-09-12 13:42:34 -0400

    fix a bug in conversion to scalar

--- tweakwcs/correctors.py
+++ tweakwcs/correctors.py
@@ -474,9 +474,10 @@ class FITSWCSCorrector(WCSCorrector):
             ra = np.atleast_1d(ra)
             dec = np.atleast_1d(dec)
         x, y = self._wcs.all_world2pix(ra, dec, 0, tolerance=1e-6, maxiter=50)
-        if not ndim:
-            x = float(x)
-            y = float(y)
+        if not ndim and np.size(x) == 1:
+            # convert to scalars:
+            x = float(x[0])
+            y = float(y[0])
         return x, y
 
     def world_to_tanp(self, ra, dec):
